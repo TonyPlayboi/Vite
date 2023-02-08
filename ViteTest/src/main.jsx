048abdf6dd5db52105703c7cd7f9b4b04a715321
TonyPlayboi <79407323+TonyPlayboi@users.noreply.github.com>
2023-02-08 05:14:03 +0100

    ViteTest

---
+++ ViteTest/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { Canvas } from '@react-three/fiber'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+  <>
+  <Canvas>
+  <mesh position={2}>
+        <boxGeometry  attach="geometry" />
+        <meshBasicMaterial attach="material" color="red" />
+    </mesh>
+  
+  </Canvas>
+  
+  </>
+)
